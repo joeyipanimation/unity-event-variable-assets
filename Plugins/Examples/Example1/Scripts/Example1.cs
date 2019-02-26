aad84ed935d5bd780b05954c432ca9435a456734
Joe <joeyipanimation@gmail.com>
2019-02-25 21:12:27 -0800

    Updating to version 0.4

---
+++ Plugins/Examples/Example1/Scripts/Example1.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using EventVariableAssets;
+
+namespace EventVariableAssets.Examples
+{
+    public class Example1 : MonoBehaviour
+    {
+        [SerializeField] GenericEvent m_resetEvent = null;
+        [SerializeField] IntVariableReference m_textSizeVariable = null;
+
+        void Start()
+        {
+            if (m_resetEvent != null)
+                m_resetEvent.Raise();
+        }
+
+        public void IncreaseTextSize()
+        {
+            if (m_textSizeVariable != null)
+                m_textSizeVariable.Value += 1;
+        }
+
+        public void DecreaseTextSize()
+        {
+            if (m_textSizeVariable != null)
+                m_textSizeVariable.Value = Mathf.Max(1, m_textSizeVariable.Value - 1);
+        }
+    }
+}
